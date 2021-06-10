fbdb2fe4d2c6931fb330988581735e15b9e06e17
Manish <manish.gowardipe@thoughtworks.com>
2021-06-10 22:09:30 +0530

    add io-ts minimal repro

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as D from 'io-ts/lib/Decoder';
+
+const aCustomDecoder: D.Decoder<unknown, { name: string; health: number }> =
+  D.struct({
+    name: D.string,
+    health: D.number,
+  });
+const bCustomDecoder: D.Decoder<
+  unknown,
+  { name: string; health: number; isAlive: boolean }
+> = D.struct({
+  name: D.string,
+  health: D.number,
+  isAlive: D.boolean,
+});
 
 interface AppProps {}
 
 function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
   useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
+    console.log(aCustomDecoder.decode({ name: 'john', health: 100 }));
+    // uncomment this line
+    console.log(bCustomDecoder.decode({ name: 'john', health: 100, isAlive: true }));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        <p>Open browser console</p>
       </header>
     </div>
   );
